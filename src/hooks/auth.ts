34ca7c1180d3aa7d1daa978f818091f136f60a86
edwardfclark <edward@streamelements.com>
2023-07-29 20:39:10 +0100

    Logout method

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -2,6 +2,7 @@ import Cookies from 'universal-cookie';
 import { useMutation } from '@tanstack/react-query';
 import { axiosClient } from '../services';
 import { enqueueSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
 
 interface LoginCredentials {
   username: string;
@@ -11,6 +12,7 @@ interface LoginCredentials {
 export function useAuth() {
   const cookies = new Cookies();
   const token = cookies.get('token');
+  const navigate = useNavigate();
 
   const loginMutation = useMutation({
     mutationFn: (credentials: LoginCredentials) => axiosClient.post('/api/login', credentials).then((res) => res.data),
@@ -19,13 +21,20 @@ export function useAuth() {
       cookies.set('token', token, { path: '/' });
       enqueueSnackbar('Login successful', { variant: 'success' });
     },
-    onError: (err) => {
+    onError: () => {
       enqueueSnackbar('Login failed', { variant: 'error' });
     },
   });
 
+  function logout() {
+    cookies.remove('token');
+    navigate('/login');
+    enqueueSnackbar('Logout successful', { variant: 'info' });
+  }
+
   return {
     token,
     login: (credentials: LoginCredentials) => loginMutation.mutate(credentials),
+    logout,
   };
 }
