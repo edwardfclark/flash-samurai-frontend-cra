74c3a0d3c77921243e8c0fad336d570a0383bcbd
edwardfclark <edward@streamelements.com>
2023-07-29 12:07:40 +0100

    Install react-query and add QueryClientProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   const defaultTheme = createTheme();
   return (
     <React.StrictMode>
-      <ThemeProvider theme={defaultTheme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={defaultTheme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 }
