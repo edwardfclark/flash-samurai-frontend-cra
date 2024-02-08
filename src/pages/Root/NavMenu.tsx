fa80113eef2d9df0a8d369f70b5bc16a5abcca90
edwardfclark <edward@streamelements.com>
2024-02-08 20:04:24 +0000

    Add dark mode toggle

--- src/pages/Root/NavMenu.tsx
+++ src/pages/Root/NavMenu.tsx
@@ -1,15 +1,29 @@
-import { AppBar, Container, Toolbar, Box, IconButton, Typography, Menu, MenuItem } from '@mui/material';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Box,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  FormControlLabel,
+  Switch,
+} from '@mui/material';
 import { useState, useEffect } from 'react';
 import { Menu as MenuIcon, Logout } from '@mui/icons-material';
 import { useAuth } from '../../hooks/useAuth';
 import { navItems } from './navItems';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocalStorage } from 'react-use';
+import { DARK_MODE_STORAGE_KEY } from '../../utils/constants';
 
 export function NavMenu() {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const { isAuthenticated, logout } = useAuth();
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const [darkMode, setDarkMode] = useLocalStorage(DARK_MODE_STORAGE_KEY, 'false');
 
   useEffect(() => {
     if (!isAuthenticated && pathname !== '/login') {
@@ -63,6 +77,18 @@ export function NavMenu() {
                   <Typography textAlign="center">{item.name}</Typography>
                 </MenuItem>
               ))}
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={darkMode === 'true'}
+                    onChange={() => setDarkMode(darkMode === 'true' ? 'false' : 'true')}
+                  />
+                }
+                label="Dark Mode"
+                sx={{
+                  px: 2,
+                }}
+              />
             </Menu>
           </Box>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
